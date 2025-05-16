9031a651fddc82ad5abe5f5ea3661ce59ae5572b
nixblick <nixblick@gmail.com>
2025-05-16 19:48:29 +0200

    debug add

--- api/index.php
+++ api/index.php
@@ -7,7 +7,10 @@
 
 // Session starten
 session_start();
-
+// Debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Einbinden der benötigten Module
 require_once 'config.php';
 require_once 'db.php';
